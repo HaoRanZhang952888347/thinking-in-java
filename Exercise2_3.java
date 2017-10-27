613482947531ad59d38a3b6293813d7bae9da41d
张皓然 <33110469+HaoRanZhang952888347@users.noreply.github.com>
2017-10-27 20:50:14 +0800

    Create Exercise2_3.java

---
+++ Exercise2_3.java
@@ -0,0 +1,15 @@
+//:one/Exercise2_3
+
+/*this first thinking in java exercise prgram
+*@author HaoRanZhang
+*/
+
+public class Exercise2_3{
+	public static void main(String[] args){
+		ATypeName a = new ATypeName();
+		System.out.println(a.i);
+	}
+}
+class ATypeName{
+	int i;
+}
