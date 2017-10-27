6a3e6bcb2f768a1591e522d8c82a201b09ebdad8
张皓然 <33110469+HaoRanZhang952888347@users.noreply.github.com>
2017-10-27 20:45:10 +0800

    Create Exercise2_2.java

---
+++ Exercise2_2.java
@@ -0,0 +1,11 @@
+//:one/Exercise2_2
+
+/*this first thinking in java exercise program
+*@author HaoRanZhang
+*/
+
+public class Exercise2_2{
+	public static void main(String[] args){
+		System.out.println("Hello,World");
+	}
+}
