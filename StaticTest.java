c983215472ac215af1e2234c02fb47158d3c7bf9
张皓然 <33110469+HaoRanZhang952888347@users.noreply.github.com>
2017-10-27 08:52:38 +0800

    Update StaticTest.java

--- StaticTest.java
+++ StaticTest.java
@@ -9,7 +9,8 @@ public class StaticTest{
 		Test test1 = new Test();
 		Test test2 = new Test();
 		++test1.i;
-		System.out.println("答案1:"+test1.i+"  答案2:"+test2.i);//test1.i和test2.i的值相同，
-		                                                        //因为static，两个对象指向同一内存地址
+		System.out.println("答案1:"+test1.i+"  答案2:"+test2.i+"  答案3:"+Test.i);
+		//test1.i和test2.i的值相同,因为static，两个对象指向同一内存地址。
+		//static变量可以直接通过类名直接调用,Test.i;
 	}
 }
