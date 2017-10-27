784704b67bddd4cb405afcfe76834fb25e379327
张皓然 <33110469+HaoRanZhang952888347@users.noreply.github.com>
2017-10-27 20:28:27 +0800

    Create Exersice2.1.java

---
+++ Exersice2.1.java
@@ -0,0 +1,13 @@
+//:one/Exercise2_1
+
+/**this first Thinking in java exercise program
+ *@author HaoRanZhang
+*/
+
+public class Exercise2_1{
+		static int i;
+		static char c;
+	public static void main(String[] args){
+		System.out.println("int型: "+i+" char型: "+c);
+	}
+}
