83fd12f8f950b07460f293f49ec83ee0a53c5cb1
张皓然 <33110469+HaoRanZhang952888347@users.noreply.github.com>
2017-10-29 09:42:47 +0800

    Create Exercise2_6.java

---
+++ Exercise2_6.java
@@ -0,0 +1,15 @@
+//:one/Exercise2_6
+
+/*this is first thinking in java exercise program
+*@author HaoRanZhang
+*/
+
+public class Exercise2_6{
+	static int storage(String s){
+		return s.length()*2;
+	}
+	
+	public static void main(String[] args){
+		System.out.println(Exercise2_6.storage(" "));
+	}
+}
